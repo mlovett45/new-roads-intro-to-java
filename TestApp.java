e1d4559733e280803f3d5f8958cd15f958ae8298
mlovett <mlovett@newroads.org>
2016-09-09 12:23:46 -0400

    First commit

---
+++ TestApp.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class TestApp{ 
+
+  //this is the entry poiny of this program 
+public static void main (String[] args){
+
+//data types in java 
+int num1 = 5;
+double num2 = 3.5;
+String world = "This is a string literal";
+ char x = 'x'; 
+  
+//create a program that takes in 3 user inputs
+//and outputs the average to the screen
+  
+  Scanner input = new Scanner(System.in);
+  System.out.println("Enter three numbers: ");
+  double num3 = input.nextDouble();
+  double num4 = input.nextDouble();
+  double num5 = input.nextDouble();
+  
+ 
+  System.out.println("The numbers you choose are "  + num3 + " " + num4 + " " + num5);
+  
+  
+  double sum = num3 + num4 + num5;
+  double average = sum / 3;
+  
+  System.out.println("This is the average of your numbers"
+                    + average);
+  
+  //this is boolean 
+  boolean flag = false;
+  
+  if(average > 2){
+    
+      flag = true;
+  }
+  
+  if (flag == true){
+      System.out.println("Your average is greater than 2");
+  }
+  
+  
+   }
+   
+ }
